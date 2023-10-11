21505c9f1a142f7e88d196e0df91ecee3096c1eb
Miguel Galicia <54015101+MichaelGaliciaZ@users.noreply.github.com>
2023-10-10 21:03:30 -0600

    API completed

--- api/main.py
+++ api/main.py
@@ -1,48 +1,34 @@
 from fastapi import FastAPI
-from pydantic import BaseModel
-import joblib
+from model_utils import MilkModel, PredictionRequest, PredictionResponse
 
-class ModelData(BaseModel):
-    pH:float
-    Temp:float
-    Taste:float
-    Odor:float
-    Fat:float
-    Turbidity:float
-    Colour:float
-
-class PredictionResponse(BaseModel):
-    milk_quality: float
-
-class MilkModel():
-    def __init__(self):
-        self.model_name='../models/gnbayes.joblib'
-        self.model=joblib.load(self.model_name)
-
-    def predict_quality(self,pH,Temp,Taste,Odor,Fat,Turbidity,Colour):
-        data_input=[[pH,Temp,Taste,Odor,Fat,Turbidity,Colour]]
-        prediction=self.model.predict(data_input)
-        probability=self.model.predict_proba(data_input).max()
-        return prediction[0],probability
-
-app=FastAPI(
+app = FastAPI(
     title="Quality Milk Prediction API",
     description="An API for predicting the quality of milk.",
     version="1.0.0",
 )
-model=joblib.load('../models/gnbayes.joblib')
+model = MilkModel()
 
-@app.post('/')
-def predict(data:ModelData):
+
+@app.post("/")
+def predict(data: PredictionRequest):
     """
     Milk Quality Predictions
     """
-    input_data=data.model_dump()
+    input_data = data.model_dump()
     print(data)
     print(input_data)
-    print("pH equal to:",input_data['pH']+5)
-    pred = model.predict([[input_data['pH'],1,2,3,4,5,6]])[0] 
-    print(pred)
-    prediction=int(pred)
+    print("pH equal to:", input_data["pH"] + 5)
+    pred, proba = model.predict_quality(
+        input_data["pH"],
+        input_data["Temp"],
+        input_data["Taste"],
+        input_data["Odor"],
+        input_data["Fat"],
+        input_data["Turbidity"],
+        input_data["Colour"],
+    )
+    print(pred, proba)
+    prediction = int(pred)
+    print(prediction)
 
-    return PredictionResponse(milk_quality=pred)
+    return PredictionResponse(milk_quality=pred, milk_quality_prob=proba)
